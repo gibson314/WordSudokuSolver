cbb1b562edb186716a97a9bae662b95915e5f6bb
Litian Ma <Litian Ma>
2016-10-21 02:54:31 -0500

    Word Sudoku Solver

---
+++ wordsudoku.py
@@ -0,0 +1,166 @@
+'''
+Wordsudoku of decoy word
+Author: Litian Ma
+'''
+
+import copy
+
+
+class Placement:
+    def __init__(self, direction, coordinate, word):
+        self.direction = direction
+        self.coordinate = coordinate
+        self.word = word
+        self.word_len = len(word)
+
+
+# AFTER CHECK. assign command into sudoku, and return a new sudoku
+def place_word(sudoku_matrix, placement):
+    if placement.direction == 'V':
+        for n in range(placement.word_len):
+            sudoku_matrix[placement.coordinate[0] + n][placement.coordinate[1]] = placement.word[n]
+    elif placement.direction == 'H':
+        for n in range(placement.word_len):
+            sudoku_matrix[placement.coordinate[0]][placement.coordinate[1] + n] = placement.word[n]
+    return sudoku_matrix
+
+
+def is_complete(sudoku_matrix):
+    for row in sudoku_matrix:
+        if '_' in row:
+            return False
+    return True
+
+
+class WordSudokuSlover:
+    direction = ['V', 'H']
+
+    def __init__(self, gridfile, wordbank):
+        self.sudoku_matrix = []
+        self.wordbank = []
+        self.trackingpath = []
+        with open(gridfile, 'rt') as grid:
+            for line in grid:
+                self.sudoku_matrix.append(list(line.strip('\n')))
+
+        with open(wordbank, 'rt') as bank:
+            for line in bank:
+                self.wordbank.append(line.strip('\n').upper())
+
+    def checkContraint(self, sudoku_matrix, placement):
+        y = placement.coordinate[0]
+        x = placement.coordinate[1]
+        if placement.direction == 'V' and y + placement.word_len <= 9:
+            for i in range(placement.word_len):
+                if placement.word[i] == sudoku_matrix[y + i][x]:
+                    continue
+                elif sudoku_matrix[y + i][x] != '_' \
+                    or placement.word[i] in sudoku_matrix[y + i] \
+                    or placement.word[i] in [sudoku_matrix[j][x] for j in range(9)] \
+                    or placement.word[i] in [sudoku_matrix[j][k]
+                                             for j in range((y+i)//3*3, (y+i)//3*3+3)
+                                             for k in range(x//3*3, x//3*3+3)]:
+                    return False
+            return True
+        elif placement.direction == 'H' and x + placement.word_len <= 9:
+            for i in range(placement.word_len):
+                if placement.word[i] == sudoku_matrix[y][x + i]:
+                    continue
+                elif sudoku_matrix[y][x + i] != '_' \
+                    or placement.word[i] in sudoku_matrix[y] \
+                    or placement.word[i] in [sudoku_matrix[j][x + i] for j in range(9)] \
+                    or placement.word[i] in [sudoku_matrix[j][k]
+                                             for j in range(y//3*3, y//3*3+3)
+                                             for k in range((x+i)//3*3, (x+i)//3*3+3)]:
+                    return False
+            return True
+        else:
+            return False
+
+    def recursive_search(self, assignment, wordbank):
+        if is_complete(assignment):
+            return assignment
+        word = self.selectWord(assignment, wordbank)
+
+        for placement in self.domainValues(word, assignment):
+
+            # if self.checkContraint(assignment, placement):
+            new_assignment = copy.deepcopy(assignment)
+            new_wordbank = copy.deepcopy(wordbank)
+            new_wordbank.remove(word)
+            new_assignment = place_word(new_assignment, placement)
+            self.trackingpath.append(placement)
+            result = self.recursive_search(new_assignment, new_wordbank)
+            if result:
+                return result
+            self.trackingpath.remove(placement)
+        return False
+
+    def selectWord(self, assignment, wordbank):
+        maxlen = 0
+        returnword = ''
+        for word in wordbank:
+            if len(word) > maxlen:
+                returnword = word
+                maxlen = len(word)
+        return returnword
+
+    def domainValues(self, word, assignment):
+        placements = []
+        for i in range(10 - len(word)):
+            # if some character already exist in the matrix
+            direction = 'V'
+            for j in range(9):
+                placement = Placement(direction, (i, j), word)
+                if self.checkContraint(assignment, placement):
+                    placements.append(placement)
+            direction = 'H'
+            for j in range(9):
+                placement = Placement(direction, (j, i), word)
+                if self.checkContraint(assignment, placement):
+                    placements.append(placement)
+        placements.sort(key=lambda p: self.count_char_exist(p, assignment), reverse=True)
+        a = [(p.direction, p.coordinate, p.word) for p in placements]
+        return placements
+
+    # given placement find maximum number of existing character in the assign
+    def count_char_exist(self, placement, assignment):
+        count = 0
+        y = placement.coordinate[0]
+        x = placement.coordinate[1]
+        origin_string = ""
+        if placement.direction == 'V':
+            origin_string = [assignment[y + i][x] for i in range(placement.word_len)]
+        elif placement.direction == 'H':
+            origin_string = [assignment[y][x + i] for i in range(placement.word_len)]
+        for i in range(placement.word_len):
+            if origin_string[i] != placement.word[i] and origin_string[i] != '_':
+                return 0
+            if origin_string[i] == placement.word[i]:
+                count += 1
+        return count
+
+    def solve(self, solution, sequence):
+        res = self.recursive_search(self.sudoku_matrix, self.wordbank)
+        if res == 0:
+            print("No Solutions for given Word Sudoku!")
+        else:
+            print("Successful!")
+            with open(solution, 'wt') as file:
+                for row in res:
+                    print(''.join(row), file=file)
+            with open(sequence, 'wt') as file:
+                for placement in self.trackingpath:
+                    print(placement.direction + "," +
+                          str(placement.coordinate[0]) + "," +
+                          str(placement.coordinate[1]) + ": " +
+                          placement.word, file=file)
+
+
+def main():
+    sudoku = WordSudokuSlover("grid1.txt", "bank1.txt")
+    sudoku.solve("solution1", "sequence1")
+
+if __name__ == '__main__':
+    main()
+
